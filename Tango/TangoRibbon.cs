0463fc0c39738fe225a89d16df1fe42a8b0c5cc5
Ramesh Bagri <rameshbagri@gmail.com>
2017-12-15 09:16:37 +0530

    Changes 201712150915

---
+++ Tango/TangoRibbon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace Tango
+{
+    public partial class TangoRibbon
+    {
+        
+        private void TangoRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+            
+        }
+
+        private void toggleButton1_Click(object sender, RibbonControlEventArgs e)
+        {
+            if (!(Globals.ThisAddIn.TaskPane.Visible = false)) { Globals.ThisAddIn.TaskPane.Visible = ((RibbonToggleButton)sender).Checked; }
+        }
+    }
+}
---
+++ Tango/TangoUserControl.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Tango
+{
+    partial class TangoUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnSelectAll = new System.Windows.Forms.Button();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnReser = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlFunctionList = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.HomePage = new System.Windows.Forms.TabPage();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.pnlFunctionList.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.HomePage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(295, 31);
+            this.panel1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(14, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(278, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnSelectAll
+            // 
+            this.btnSelectAll.Location = new System.Drawing.Point(3, 11);
+            this.btnSelectAll.Name = "btnSelectAll";
+            this.btnSelectAll.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectAll.TabIndex = 0;
+            this.btnSelectAll.Text = "Select All";
+            this.btnSelectAll.UseVisualStyleBackColor = true;
+            this.btnSelectAll.Click += new System.EventHandler(this.btnSelectAll_Click);
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(115, 11);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 1;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // btnReser
+            // 
+            this.btnReser.Location = new System.Drawing.Point(213, 11);
+            this.btnReser.Name = "btnReser";
+            this.btnReser.Size = new System.Drawing.Size(75, 23);
+            this.btnReser.TabIndex = 2;
+            this.btnReser.Text = "Reset";
+            this.btnReser.UseVisualStyleBackColor = true;
+            this.btnReser.Click += new System.EventHandler(this.btnReser_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnReser);
+            this.panel2.Controls.Add(this.btnExecute);
+            this.panel2.Controls.Add(this.btnSelectAll);
+            this.panel2.Location = new System.Drawing.Point(11, 460);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(295, 44);
+            this.panel2.TabIndex = 2;
+            // 
+            // pnlFunctionList
+            // 
+            this.pnlFunctionList.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlFunctionList.Controls.Add(this.tabControl1);
+            this.pnlFunctionList.Location = new System.Drawing.Point(4, 41);
+            this.pnlFunctionList.Name = "pnlFunctionList";
+            this.pnlFunctionList.Size = new System.Drawing.Size(302, 413);
+            this.pnlFunctionList.TabIndex = 3;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.HomePage);
+            this.tabControl1.Location = new System.Drawing.Point(7, 16);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(288, 385);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // HomePage
+            // 
+            this.HomePage.Controls.Add(this.btnProcess);
+            this.HomePage.Controls.Add(this.checkBox6);
+            this.HomePage.Controls.Add(this.checkBox5);
+            this.HomePage.Controls.Add(this.checkBox4);
+            this.HomePage.Controls.Add(this.checkBox3);
+            this.HomePage.Controls.Add(this.checkBox2);
+            this.HomePage.Controls.Add(this.checkBox1);
+            this.HomePage.Location = new System.Drawing.Point(4, 22);
+            this.HomePage.Name = "HomePage";
+            this.HomePage.Padding = new System.Windows.Forms.Padding(3);
+            this.HomePage.Size = new System.Drawing.Size(280, 359);
+            this.HomePage.TabIndex = 0;
+            this.HomePage.Text = "Home";
+            this.HomePage.UseVisualStyleBackColor = true;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcess.Location = new System.Drawing.Point(49, 207);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(123, 28);
+            this.btnProcess.TabIndex = 27;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click_1);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox6.Location = new System.Drawing.Point(49, 170);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(103, 21);
+            this.checkBox6.TabIndex = 26;
+            this.checkBox6.Text = "Function 6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox5.Location = new System.Drawing.Point(49, 143);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(103, 21);
+            this.checkBox5.TabIndex = 25;
+            this.checkBox5.Text = "Function 5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox4.Location = new System.Drawing.Point(49, 116);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(103, 21);
+            this.checkBox4.TabIndex = 24;
+            this.checkBox4.Text = "Function 4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.Location = new System.Drawing.Point(49, 89);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(103, 21);
+            this.checkBox3.TabIndex = 23;
+            this.checkBox3.Text = "Function 3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(49, 62);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(152, 21);
+            this.checkBox2.TabIndex = 22;
+            this.checkBox2.Text = "Wicket => Bucket";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(49, 35);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(132, 21);
+            this.checkBox1.TabIndex = 21;
+            this.checkBox1.Text = "Find / Replace";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // TangoUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnlFunctionList);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "TangoUserControl";
+            this.Size = new System.Drawing.Size(313, 507);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.pnlFunctionList.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.HomePage.ResumeLayout(false);
+            this.HomePage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnSelectAll;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnReser;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel pnlFunctionList;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage HomePage;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
